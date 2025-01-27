222613d82c2f8acefb02881bd91c7388e6d8c777
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2025-01-27 19:39:43 +0100

    feat(/): Added code to get commits from a repository

--- code/giteaApiAttack.py
+++ code/giteaApiAttack.py
@@ -1,28 +1,14 @@
-import requests as rq
-
-# Method to get the repositories of the authenticated user
-def get_repositories(base_url, headers):
-    url = f"{base_url}/user/repos"
-    return rq.get(url, headers=headers)
-
-def search_repositories(base_url, headers, params):
-    url = f"{base_url}/repos/search"
-    return rq.get(url, headers=headers, params=params)
-
-def get_user_by_username(base_url, headers, name):
-    url = f"{base_url}/users/{name}"
-    return rq.get(url, headers=headers, params={"username": name})
-
+from ApiInterrogator import ApiInterrogator
+from datetime import datetime
 
 if __name__ == "__main__":
+
     URL = "https://gitea.com/api/v1"
     TOKEN = "82b9c59c55aa72fabd8bdde707ccdfd165db8160"
-    HEADERS = {
-        "Authorization": f"token {TOKEN}",
-        "Content-Type": "application/json"
-    }
 
-    response = get_repositories(URL, HEADERS)
+    api_interrogator = ApiInterrogator(URL, TOKEN)
+
+    response = api_interrogator.get_authenticated_user_repositories()
     if response.status_code == 200:
         if response.json():
             print("Repositories:")
@@ -32,7 +18,7 @@ if __name__ == "__main__":
         print("Error:", response.status_code, response.text)
 
     # Get gitea user information
-    response = get_user_by_username(URL, HEADERS, "gitea")
+    response = api_interrogator.get_userid_by_username("gitea")
     userId = 0
     if response.status_code == 200:
         if response.json():
@@ -41,21 +27,50 @@ if __name__ == "__main__":
     else:
         print("Error:", response.status_code, response.text)
 
-    # Search repositories
-    params = {
-        "uid": userId,
-        "archived": False,
-        "exclusive": True,
-        "sort": "stars",
-        "order": "desc"
-    }
 
-    response = search_repositories(URL, HEADERS, params)
-    if response.status_code == 200:
-        if response.json():
-            data = response.json()['data']
-            print("Repositories:")
-            for repo in data:
-                print(f"  - {repo['name']}")
-    else:
-        print("Error:", response.status_code, response.text)
+    # Search repositories
+
+    repositories = []
+
+    for i in range(1, 10):
+        print(f"Page {i}")
+        params = {
+            'sort': 'stars',
+            'order': 'desc',
+            'limit': 20,
+            'page': i
+        }
+
+        response = api_interrogator.search_repositories(params)
+        if response.status_code == 200:
+            if response.json():
+                data = response.json()['data']
+
+                for repo in data:
+
+                    repo_last_updated = datetime.strptime(repo['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+                    comparison_date = datetime(2021, 1, 1)
+
+                    if repo_last_updated > comparison_date:
+                        repositories.append(repo)
+
+
+        else:
+            print("Error:", response.status_code, response.text)
+            break
+
+    print("Repositories:")
+    print(len(repositories))
+
+    commits = []
+    for repo in repositories:
+        response = api_interrogator.get_all_commits(repo['owner']['login'], repo['name'], {})
+        if response.status_code == 200:
+            if response.json():
+                commits.append((repo['name'], response.json()))
+        else:
+            print("Error:", response.status_code, response.text)
+
+    print("Commits:")
+    for commit in commits[0][1]:
+        print(f"  - {commit['files']}")

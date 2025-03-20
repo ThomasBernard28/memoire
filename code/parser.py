a7c565c4b5e3e3509dc3d4a1f542c8c819350ce5
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2025-03-20 16:26:10 +0100

    Small changes

--- code/parser.py
+++ code/parser.py
@@ -11,9 +11,9 @@ def parse_workflow(file_path):
 
     try:
         with open(file_path, 'r', encoding="utf-8") as f:
-            yaml_data = yaml.safe_load(f)
+            yaml_data = yaml.load(f)
             print(yaml_data)
-    except yaml.YAMLError as e:
+    except Exception as e:
         print(f"Error while parsing YAML: {e}")
         return None
 
@@ -35,15 +35,26 @@ def parse_workflow(file_path):
         workflow_info["events"] = event_data
     # else if there is only one trigger defined as a string, use it as is
     else:
-        workflow_info["events"] = [event_data] if isinstance(event_data, str) else event_data
+        workflow_info["events"] = [event_data] if isinstance(event_data, str) else []
 
     # Code to analyze and extract information about jobs and events
     for job_name, job_data in yaml_data.get("jobs", {}).items():
         steps = job_data.get("steps", [])
+
+        # Get step details
+        step_details = []
+        for step in steps:
+            step_details.append({
+                "name" : step.get("name", "Unkmown"),
+                "uses" : step.get("uses", None),
+                "run" : step.get("run", None)
+            })
+
         workflow_info["jobs"][job_name] = {
             "steps_count": len(steps),
             "uses_github_actions": any("uses" in step for step in steps),
-            "uses_commands": any("run" in step for step in steps)
+            "uses_commands": any("run" in step for step in steps),
+            "step_details": step_details
         }
 
     return workflow_info
@@ -80,6 +91,9 @@ if __name__ == "__main__":
                 print(f"    - Nombre de steps: {details['steps_count']}")
                 print(f"    - Utilise GitHub Actions: {details['uses_github_actions']}")
                 print(f"    - Utilise des commandes shell: {details['uses_commands']}")
+                print(f"    - Détails des steps :")
+                for step in details["step_details"]:
+                    print(f"      - {step['name']}: uses={step['uses']}, run={step['run']}")
 
     #count_workflows_per_year(df)
 
